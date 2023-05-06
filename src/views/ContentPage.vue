8c0257c73cb203473196d068d6bfaf05781d1b04
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2023-05-05 22:10:36 -0500

    feat: Add EditElement to options for display

--- src/views/ContentPage.vue
+++ src/views/ContentPage.vue
@@ -1,12 +1,16 @@
 <script setup>
-import FormElement from '../components/FormElement.vue'
+import CreateElement from '../components/CreateElement.vue'
+import EditElement from '../components/EditElement.vue'
 import ViewElement from '../components/ViewElement.vue'
 </script>
 
 <template>
   <div class="w-full bg-emerald-300 p-5">
-    <h2 class="center mx-auto" v-if="typeView === 0">Pagina de inicio</h2>
-    <FormElement v-else-if="$store.state.typeView === 1" />
-    <ViewElement v-else-if="$store.state.typeView === 2" :data="$store.getters.getElement" />
+    <div v-if="$store.state.typeView == 0">
+      <h2>Pagina de inicio</h2>
+    </div>
+    <CreateElement v-else-if="$store.state.typeView == 1" />
+    <ViewElement v-else-if="$store.state.typeView == 2" :data="$store.getters.getElement" />
+    <EditElement v-else-if="$store.state.typeView == 3" :data="$store.getters.getElementToEdit" />
   </div>
 </template>
