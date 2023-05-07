3b01e57d4d43cc591af75cc4a0e0a6d5413dc321
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2023-05-06 21:55:26 -0500

    feat: Implement delete method with index

--- src/components/SidebarList.vue
+++ src/components/SidebarList.vue
@@ -13,22 +13,22 @@ const editElement = (id) => {
   store.state.typeView = 3
 }
 
-const deleteElement = (id) => {
-  store.commit('deleteElement', id)
+const deleteElement = (index) => {
+  store.commit('deleteElement', index)
 }
 </script>
 
 <template>
   <ul>
     <li
-      v-for="d in dataElements"
+      v-for="(d, index) in dataElements"
       :key="d.id"
       class="flex justify-between border-2 border-white p-2 px-4 my-2"
     >
       <span class="cursor-pointer" @click="changeState(d.id)">{{ d.name || d.id }}</span>
       <span>
         <button class="mx-4" @click="editElement(d.id)">Editar</button>
-        <button @click="deleteElement(d.id)">Eliminar</button>
+        <button @click="deleteElement(index)">Eliminar</button>
       </span>
     </li>
   </ul>
