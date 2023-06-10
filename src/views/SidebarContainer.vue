3ca0d9c6bb89c82b5e12610d559e57ba4a79b986
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2023-06-09 22:14:54 -0500

    feat: Implement dropdown menu

--- src/views/SidebarContainer.vue
+++ src/views/SidebarContainer.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { ref, reactive, computed } from 'vue'
+
 import SidebarList from '../components/SidebarList.vue'
+import DropDownMenu from '../components/custom/DropDownMenu.vue'
+
 import store from '../store'
 import { CREATE_STATE } from '../constants'
 
@@ -10,13 +13,10 @@ const selectedType = ref('0')
 const input = ref('')
 const filterBtn = ref(true)
 const data = reactive({ value: props.data })
+const customSelectProperty = reactive({ name: 'Seleccione una opcion' })
 
 const changeState = () => {
   store.state.typeView = CREATE_STATE
-}
-
-const filterList = () => {
-  filterBtn.value = false
 }
 
 const inputChange = () => {
@@ -28,6 +28,7 @@ const defaultState = () => {
   data.value = props.data
   selectedType.value = '0'
   input.value = ''
+  customSelectProperty.name = 'Seleccione una opcion'
 }
 
 const filteredList = computed(() => {
@@ -45,21 +46,44 @@ const filteredList = computed(() => {
   }
   return data.value
 })
+
+const selectOption = (id) => {
+  selectedType.value = id
+  filterBtn.value = false
+}
 </script>
 
 <template>
-  <header class="w-[25%] min-w-[20rem] max-w-xs bg-amber-300 p-3">
-    <input type="text" v-model="input" placeholder="Busqueda" @input="inputChange" />
-    <button class="bg-sky-700 text-white px-2" @click="changeState">+ Nuevo</button>
-    <br />
-    <label for="type_id">Tipo de elemento</label>
-    <select name="" id="type_id" @change="filterList" v-model="selectedType">
-      <option value="0" hidden>Seleccione</option>
-      <option v-bind:value="typ.id" :key="typ.id" v-for="typ in props.types">{{ typ.name }}</option>
-    </select>
-    <button class="bg-sky-700 text-white px-2" @click="defaultState" :disabled="filterBtn">
-      Borrar filtros
-    </button>
+  <header class="hidden md:block w-[35%] min-w-[18rem] max-w-xs bg-gray-700 text-zinc-300 p-3">
+    <label for="busqueda_id">Ingrese el término a buscar:</label>
+    <input
+      type="text"
+      id="busqueda_id"
+      class="w-full my-2 border-2 border-solid border-[#FFF2] bg-[#2a2f3b] rounded p-3 text-base outline-0"
+      v-model="input"
+      placeholder="Búsqueda"
+      @input="inputChange"
+    />
+
+    <label for="type_id" class="block mt-2">Tipo de elemento</label>
+
+    <DropDownMenu :data="types" @select-option="selectOption" :selected="customSelectProperty" />
+
+    <div class="flex justify-end my-4">
+      <button
+        class="bg-sky-700 text-white px-3 py-1 mx-2 hover:opacity-75 rounded"
+        @click="changeState"
+      >
+        Nuevo
+      </button>
+      <button
+        class="bg-red-700 text-white px-3 py-1 disabled:opacity-50 disabled:cursor-not-allowed hover:opacity-75 rounded"
+        @click="defaultState"
+        :disabled="filterBtn"
+      >
+        Borrar filtros
+      </button>
+    </div>
     <SidebarList :dataElements="filteredList" />
   </header>
 </template>
