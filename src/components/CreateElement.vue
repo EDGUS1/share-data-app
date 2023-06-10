536af4ffa7e10f07c80ed0bd20969e1b57f9d8d0
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2023-06-09 22:06:12 -0500

    feat: Change html select to custom dropdown

--- src/components/CreateElement.vue
+++ src/components/CreateElement.vue
@@ -1,23 +1,31 @@
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 
 import UrlForm from './Forms/UrlForm.vue'
 import InfoForm from './Forms/InfoForm.vue'
 import CodeForm from './Forms/CodeForm.vue'
 import FileForm from './Forms/FileForm.vue'
+import DropDownMenu from './custom/DropDownMenu.vue'
 import { TYPES, TYPE_LINK_ID, TYPE_CODE_ID, TYPE_INFO_ID, TYPE_FILE_ID } from '../constants'
 
-const selectOption = ref(0)
+const selectedType = ref('0')
+const customSelectProperty = reactive({ name: 'Seleccione una opcion' })
+
+const selectOption = (id) => {
+  selectedType.value = id
+}
 </script>
 
 <template>
-  <label for="option_type_id">Seleccione el tipo de elemento</label>
-  <select name="" id="option_type_id" v-model="selectOption">
-    <option value="0" hidden>Seleccione</option>
-    <option v-bind:value="typ.id" :key="typ.id" v-for="typ in TYPES">{{ typ.name }}</option>
-  </select>
-  <UrlForm v-if="selectOption == TYPE_LINK_ID" :edit="false" />
-  <InfoForm v-else-if="selectOption == TYPE_INFO_ID" :edit="false" />
-  <CodeForm v-else-if="selectOption == TYPE_CODE_ID" :edit="false" />
-  <FileForm v-else-if="selectOption == TYPE_FILE_ID" :edit="false" />
+  <div>
+    <div class="my-3 text-black">
+      <label for="option_type_id" class="text-white">Seleccione el tipo de elemento</label>
+
+      <DropDownMenu :data="TYPES" @select-option="selectOption" :selected="customSelectProperty" />
+    </div>
+    <UrlForm v-if="selectedType == TYPE_LINK_ID" :edit="false" />
+    <InfoForm v-else-if="selectedType == TYPE_INFO_ID" :edit="false" />
+    <CodeForm v-else-if="selectedType == TYPE_CODE_ID" :edit="false" />
+    <FileForm v-else-if="selectedType == TYPE_FILE_ID" :edit="false" />
+  </div>
 </template>
